edefe4b485ebd26b9bf888460f0388fe94897fde
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-01 11:00:46 +0530

    Added nav logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,28 @@
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Image from "react-bootstrap/Image";
 
 export default function Header() {
   return (
     <Navbar expand="lg" bg="primary" data-bs-theme="dark">
       <Container fluid>
-        <Navbar.Brand href="#">Flikart</Navbar.Brand>
+        <Navbar.Brand href="#" className="d-flex  flex-column">
+          <a href="/">
+            <Image src="images/flipkart-logo.png" className="w-25" />
+          </a>
+          <a href="/" className="explore-anchor">
+            Explore <span>Plus <Image src="images/flipkart-plus-star.png"/></span>
+          </a>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
@@ -48,4 +55,3 @@ export default function Header() {
     </Navbar>
   );
 }
-
