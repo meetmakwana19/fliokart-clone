57da3183c81e2001fb2d3227c2ddea1dd1375ff0
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-01 17:39:09 +0530

    BIg footer added

--- src/components/BigFooter.js
+++ src/components/BigFooter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BigFooter() {
   return (
-    <div>
+    <div className="big-footer border">
       <strong>
         Top Stories: <span>Brand Directory</span>
       </strong>
@@ -20,65 +20,200 @@ export default function BigFooter() {
         coversFlipkart QuickAmpere MagnusFlipkart Help CentreOnline Boat
         StoreCovers from Rs 99Flipkart Track OrdersFlipkart Manage
         OrdersFlipkart Return OrdersFlipkart Gift Cards StoreFlipkart Axis Bank
-        Credit CardFlipkart Pay Later MOBILES:iPhone 12 64GBiPhone 12 Pro
-        512GBiPhone 12 128GBVivo Y91iVivo Y11Vivo Y15Nothing PhoneVivo Y50Vivo
-        Y12Reno2 FOppo A12Nothing Phone 5gGoogle Pixel 5g PhoneMotorola 5g
-        PhoneRealme 5g SmartphoneApple 5g PhoneIqoo 5g PhonesOneplus 5g
-        PhonesVivo 5g PhonesOppo 5g Smart PhonesOppo F15Oppo A31Samsung
-        A71Samsung A51Samsung A31Realme X2iPhone 11iPhone 11 ProMobile
-        CoverMobile Offersiphone x4G MobileNokia MobileSamsung MobileiphoneOppo
-        MobileVivo Mobile CAMERA:GoPro Action CameraNikon CameraCanon CameraSony
-        CameraCanon DSLRNikon DSLR LAPTOPS:MacBook Pro M2Premium LaptopASUS ROG
-        Strix SCAR 16 (2023) Core i9 13th GenASUS ROG Zephyrus M16 (2023) Core
-        i9 13th GenASUS Zenbook 14 OLED (2022)Realme Book Prime Core i5 11th
-        GenMicrosoft Surface Go Pentium 128GBBranded LaptopsApple LaptopsAcer
-        LaptopsLenovo LaptopsDell LaptopsAsus LaptopsHP LaptopsGaming Laptops2
-        in 1 LaptopsLaptopsDell latest laptops 2022HP latest laptops 2022Infinix
-        INBook Y1 PlusSAMSUNG Galaxy Book312th Gen Intel Core Laptops TVS:Nokia
-        TVPanasonic TVThomson TVMi X ProRealme TVMotorola TVOnePlus TVsLG
-        TVTVSony TVSamsung TVAndroid TelevisionIffalcon TvMi TV LARGE
-        APPLIANCES:TV & Appliances End of Season SaleTelevisionWashing
+        Credit CardFlipkart Pay Later
+      </p>
+      <p>
+        MOBILES:iPhone 12 64GBiPhone 12 Pro 512GBiPhone 12 128GBVivo Y91iVivo
+        Y11Vivo Y15Nothing PhoneVivo Y50Vivo Y12Reno2 FOppo A12Nothing Phone
+        5gGoogle Pixel 5g PhoneMotorola 5g PhoneRealme 5g SmartphoneApple 5g
+        PhoneIqoo 5g PhonesOneplus 5g PhonesVivo 5g PhonesOppo 5g Smart
+        PhonesOppo F15Oppo A31Samsung A71Samsung A51Samsung A31Realme X2iPhone
+        11iPhone 11 ProMobile CoverMobile Offersiphone x4G MobileNokia
+        MobileSamsung MobileiphoneOppo MobileVivo Mobile
+      </p>
+      <p>
+        CAMERA:GoPro Action CameraNikon CameraCanon CameraSony CameraCanon
+        DSLRNikon DSLR
+      </p>
+      <p>
+        LAPTOPS:MacBook Pro M2Premium LaptopASUS ROG Strix SCAR 16 (2023) Core
+        i9 13th GenASUS ROG Zephyrus M16 (2023) Core i9 13th GenASUS Zenbook 14
+        OLED (2022)Realme Book Prime Core i5 11th GenMicrosoft Surface Go
+        Pentium 128GBBranded LaptopsApple LaptopsAcer LaptopsLenovo LaptopsDell
+        LaptopsAsus LaptopsHP LaptopsGaming Laptops2 in 1 LaptopsLaptopsDell
+        latest laptops 2022HP latest laptops 2022Infinix INBook Y1 PlusSAMSUNG
+        Galaxy Book312th Gen Intel Core Laptops
+      </p>
+      <p>
+        TVS:Nokia TVPanasonic TVThomson TVMi X ProRealme TVMotorola TVOnePlus
+        TVsLG TVTVSony TVSamsung TVAndroid TelevisionIffalcon TvMi TV
+      </p>
+      <p>
+        LARGE APPLIANCES:TV & Appliances End of Season SaleTelevisionWashing
         MachinesRefrigeratorsAir ConditionersElectric CookersElectric
         Jug(Heater) / Travel KettlesInduction CooktopsInverters /
         stabilizerIrons / Iron BoxMixer Grinder JuicerWet
         GrindersChimneysMicrowave OvensVacuum CleanersWater PurifierFanMarQ
-        Customer Care CLOTHING:Green bridal lehengaBridal BlouseHalf saree
-        blouse designsDesigner blousesBlouse designsMen ShirtsPlain suit with
-        heavy dupattaHalf saree for kidsCotton saree blouse designsKurta
-        PajamaMen T-ShirtsNew blouse designJeansBridal designer blouseSimple
-        aari work blouse designsBridal banarasi sareesSareeHigh waist
-        pantiesHooded jacket womenDressesHoodie tshirts for menOne piecesHip hop
-        cap for menGroom wedding sherwaniDesigner Salwar SuitsBraAari work
-        blouse hand designsCotton simple blouse designsBanarasi saree blouse
-        designsDesigner KurtisStylish blouse astin designTrack PantBlouse neck
-        designsMen KurtasV neck blouse designsGym WearParty DressesPalazzo
-        SuitsBoys ClothingGlovesNightyMaxi DressesAnarkaliGownsCulottesSalwar
+        Customer Care
+      </p>
+      <p>
+        CLOTHING:Green bridal lehengaBridal BlouseHalf saree blouse
+        designsDesigner blousesBlouse designsMen ShirtsPlain suit with heavy
+        dupattaHalf saree for kidsCotton saree blouse designsKurta PajamaMen
+        T-ShirtsNew blouse designJeansBridal designer blouseSimple aari work
+        blouse designsBridal banarasi sareesSareeHigh waist pantiesHooded jacket
+        womenDressesHoodie tshirts for menOne piecesHip hop cap for menGroom
+        wedding sherwaniDesigner Salwar SuitsBraAari work blouse hand
+        designsCotton simple blouse designsBanarasi saree blouse designsDesigner
+        KurtisStylish blouse astin designTrack PantBlouse neck designsMen
+        KurtasV neck blouse designsGym WearParty DressesPalazzo SuitsBoys
+        ClothingGlovesNightyMaxi DressesAnarkaliGownsCulottesSalwar
         SuitsKurtisDesigner SareesLeggingsShortsGeorgette SareesEthnic
         WearBriefs & TrunksNike WatchesWomen henleyuppada pattu sareesBlouse
         back designLeopard dressesSleeveless shrugsLatest blouse designsSimple
-        Blouse designs FOOTWEAR:ShoesAdidas ShoesReebok ShoesNike ShoesPuma
-        ShoesBootsBata ShoesWoodland ShoesSkechers ShoesSneakersWomens
-        BootsSports ShoesLoafersSandalsLotto Sports ShoesCasual ShoesWomens
-        Skechers ShoesAsics Sports ShoesFormal ShoesSchool Shoes
-        GROCERIES:PhonePe Grocery VoucherHand WashSoapCashew NutsSunflower
-        OilEggsToilet CleanerHarpic Toilet CleanerDettol SoapMustard
-        OilBiscuitsCheesePatanjali AttaFortune OilAashirvaad AttaTea BEST
-        SELLING ON FLIPKART:Fire-Boltt Ninja Calling Pro Bluetooth Calling
+        Blouse designs
+      </p>
+      <p>
+        FOOTWEAR:ShoesAdidas ShoesReebok ShoesNike ShoesPuma ShoesBootsBata
+        ShoesWoodland ShoesSkechers ShoesSneakersWomens BootsSports
+        ShoesLoafersSandalsLotto Sports ShoesCasual ShoesWomens Skechers
+        ShoesAsics Sports ShoesFormal ShoesSchool Shoes
+      </p>
+      <p>
+        BEST SELLING ON FLIPKART:Fire-Boltt Ninja Calling Pro Bluetooth Calling
         Smartwatch 1.69 inch HD Display SmartwatchBest Gas GeyserKitchen
         GeyserNutri BlendersPortable Air CoolerBest Air CoolerBagsHitachi
         Refrigerator 3 DoorBooksToysCandlesHelmetsWall ClocksBaby
-        FoodChocolatesCyclesCalculatorsLipsticksMaskVertiv UPSFastrack
-        WatchesWalletsEarringsGold CoinsRealme Pad MiniHandbagsconekt SW2
-        SmartwatchMivi DuoPods a350Speaker Cleaner
-        FURNITURE:FurnitureSofasBedsDining setsWardrobesMattressesTV
-        UnitsTablesChairsShelvesBean BagsOffice ChairsComputer TableOffice
-        TablesRed SofaWakefit BedsWhite SofaWakefit MattressGreen SofaBlack
-        SofaBrown Sofa BGMH:Whey
-        ProteinHomeopathyCricketCyclesFootballsTreadmillsChristmas GiftsFitness
-        AccessoriesOnline GuitarBooks StoreMusical Instrument StoreDabur
-        ChyawanprashBaidyanath ChyawanprashEnergy DrinksToysMilk Drink
-        MixesRakhiChyawanprashIndian FlagProtein Supplements
       </p>
+
+      <br />
+      <br />
+
+      <h6>
+        <strong>Flipkart: The One-stop Shopping Destination</strong>
+      </h6>
+      <p>
+        <small>
+          E-commerce is revolutionizing the way we all shop in India. Why do you
+          want to hop from one store to another in search of the latest phone
+          when you can find it on the Internet in a single click? Not only
+          mobiles. Flipkart houses everything you can possibly imagine, from
+          trending electronics like laptops, tablets, smartphones, and mobile
+          accessories to in-vogue fashion staples like shoes, clothing and
+          lifestyle accessories; from modern furniture like sofa sets, dining
+          tables, and wardrobes to appliances that make your life easy like
+          washing machines, TVs, ACs, mixer grinder juicers and other
+          time-saving kitchen and small appliances; from home furnishings like
+          cushion covers, mattresses and bedsheets to toys and musical
+          instruments, we got them all covered. You name it, and you can stay
+          assured about finding them all here. For those of you with erratic
+          working hours, Flipkart is your best bet. Shop in your PJs, at night
+          or in the wee hours of the morning. This e-commerce never shuts down.
+          What's more, with our year-round shopping festivals and events, our
+          prices are irresistible. We're sure you'll find yourself picking up
+          more than what you had in mind. If you are wondering why you should
+          shop from Flipkart when there are multiple options available to you,
+          well, the below will answer your question.
+        </small>
+      </p>
+      <h6>
+        <strong>Flipkart Plus</strong>
+      </h6>
+      <p>
+        <small>
+          A world of limitless possibilities awaits you - Flipkart Plus was
+          kickstarted as a loyalty reward programme for all its regular
+          customers at zero subscription fee. All you need is 500 supercoins to
+          be a part of this service. For every 100 rupees spent on Flipkart
+          order, Plus members earns 4 supercoins & non-plus members earn 2
+          supercoins. Free delivery, early access during sales and shopping
+          festivals, exchange offers and priority customer service are the top
+          benefits to a Flipkart Plus member. In short, earn more when you shop
+          more!
+        </small>
+      </p>
+
+      <p>
+        <small>
+          What's more, you can even use the Flipkart supercoins for a number of
+          exciting services, like: An annual Zomato Gold membership An annual
+          Hotstar Premium membership 6 months Gaana plus subscription Rupees 550
+          instant discount on flights on ixigo Check out
+          https://www.flipkart.com/plus/all-offers for the entire list. Terms
+          and conditions apply.
+        </small>
+      </p>
+
+      <h6>
+        <strong>No Cost EMI</strong>
+      </h6>
+      <p>
+        <small>
+          In an attempt to make high-end products accessible to all, our No Cost
+          EMI plan enables you to shop with us under EMI, without shelling out
+          any processing fee. Applicable on select mobiles, laptops, large and
+          small appliances, furniture, electronics and watches, you can now shop
+          without burning a hole in your pocket. If you've been eyeing a product
+          for a long time, chances are it may be up for a no cost EMI. Take a
+          look ASAP! Terms and conditions apply.
+        </small>
+      </p>
+
+      <h6>
+        <strong>What Can You Buy From Flipkart?</strong>
+      </h6>
+      <h6>
+        <strong>Electronic Devices and Accessories</strong>
+      </h6>
+      <p>
+        <small>
+          When it comes to laptops, we are not far behind. Filter among dozens
+          of super-fast operating systems, hard disk capacity, RAM, lifestyle,
+          screen size and many other criterias for personalized results in a
+          flash. All you students out there, confused about what laptop to get?
+          Our Back To College Store segregates laptops purpose wise (gaming,
+          browsing and research, project work, entertainment, design,
+          multitasking) with recommendations from top brands and industry
+          experts, facilitating a shopping experience that is quicker and
+          simpler.
+        </small>
+      </p>
+      <p>
+        <small>
+          Photography lovers, you couldn't land at a better page than ours.
+          Cutting-edge DSLR cameras, ever reliable point-and-shoot cameras,
+          millennial favourite instant cameras or action cameras for adventure
+          junkies: our range of cameras is as much for beginners as it is for
+          professionals. Canon, Nikon, GoPro, Sony, and Fujifilm are some big
+          names you'll find in our store. Photography lovers, you couldn't land
+          at a better page than ours. Cutting-edge DSLR cameras, ever reliable
+          point-and-shoot cameras, millennial favourite instant cameras or
+          action cameras for adventure junkies: our range of cameras is as much
+          for beginners as it is for professionals. Canon, Nikon, GoPro, Sony,
+          and Fujifilm are some big names you'll find in our store.
+        </small>
+      </p>
+      <p>
+        <small>
+          Turn your home into a theatre with a stunning surround sound system.
+          Choose from our elaborate range of Sony home theatres, JBL soundbars
+          and Philips tower speakers for an experience to remember.
+        </small>
+      </p>
+      <p>
+        <small>
+          How about jazzing up your phone with our quirky designer cases and
+          covers? Our wide-ranging mobile accessories starting from headphones,
+          power banks, memory cards, mobile chargers, to selfie sticks can prove
+          to be ideal travel companions for you and your phone; never again
+          worry about running out of charge or memory on your next vacation.
+        </small>
+      </p>
+
+      <h6><strong>Lifestyle</strong></h6>
+      <p><small>Flipkart, 'India ka Fashion Capital', is your one-stop fashion destination for anything and everything you need to look good. Our exhaustive range of Western and Indian wear, summer and winter clothing, formal and casual footwear, bridal and artificial jewellery, long-lasting make-up, grooming tools and accessories are sure to sweep you off your feet. Shop from crowd favourites like Vero Moda, Forever 21, Only, Arrow, Woodland, Nike, Puma, Revlon, Mac, and Sephora among dozens of other top-of-the-ladder names. From summer staple maxi dresses, no-nonsense cigarette pants, traditional Bandhani kurtis to street-smart biker jackets, you can rely on us for a wardrobe that is up to date. Explore our in-house brands like Metronaut, Anmi, and Denizen, to name a few, for carefully curated designs that are the talk of the town. Get ready to be spoiled for choice.Festivals, office get-togethers, weddings, brunches, or nightwear - Flipkart will have your back each time.</small></p>
+
+      <p><small>Any more.....</small></p>
     </div>
   );
 }
