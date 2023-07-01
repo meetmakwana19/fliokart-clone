eaf1b5f556381d95a7e09bc018e0e2eee2cf5b9c
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-01 16:51:19 +0530

    Added one more RowDIv

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Header/>
       <SubHeader/>
       <CarouselBanner/>
+      <RowDiv title="End of Season Bestsellers" bg_image="https://rukminim1.flixcart.com/fk-p-flap/278/278/image/5824d7a002e85518.jpg?q=90"/>
       <RowDiv title="Top Offers" bg_image="https://rukminim1.flixcart.com/fk-p-flap/278/278/image/0b22f4bdbe5b032a.jpg?q=90"/>
       <RowDiv title="Today's Fashion Deals" bg_image="https://rukminim1.flixcart.com/flap/278/278/image/e84ecf024bdc49f4.jpg?q=90"/>
     </div>
