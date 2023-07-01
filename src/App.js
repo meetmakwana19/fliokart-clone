99382243d85e57469ddf38b3f7be95e60e6a9fce
meetmakwana19 <meetmakwana203@gmail.com>
2023-07-01 17:16:56 +0530

    Added big footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SubHeader from "./components/SubHeader";
 import CarouselBanner from "./components/CarouselBanner";
 import RowDiv from "./components/RowDiv";
 import OfferCards from "./components/OfferCards";
+import BigFooter from "./components/BigFooter";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <RowDiv title="Top Offers" bg_image="https://rukminim1.flixcart.com/fk-p-flap/278/278/image/0b22f4bdbe5b032a.jpg?q=90"/>
       <OfferCards/>
       <RowDiv title="Today's Fashion Deals" bg_image="https://rukminim1.flixcart.com/flap/278/278/image/e84ecf024bdc49f4.jpg?q=90"/>
+      <BigFooter/>
     </div>
   );
 }
---
+++ src/components/BigFooter.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+export default function BigFooter() {
+  return (
+    <div>
+      <strong>
+        Top Stories: <span>Brand Directory</span>
+      </strong>
+      <br />
+      <p>
+        MOST SEARCHED FOR ON FLIPKART:One PiecesFashion EOSS SaleTrend
+        StopFlipkart Axis Bank Super Elite Credit card5G Mobile PhonesPrimebook
+        LaptopsMoto Edge 40Bounce Infinity E1Buy Laptop on EMIInfinix Zero Book
+        UltraOPPO Reno8 5GOPPO Reno8 Pro 5GFresh Flower BouquetElectric
+        CyclesBadmintonBouquetLaptop Repair ServicesAC Cleaning ServicesOneplus
+        MonitorsGrievance RedressalMobile Repair ServicesSell Old Mobile
+        PhonesPremium LaptopsElectronics StorePhone coverInfinix INBook Y1
+        PlusOkaya Electric Vehiclesbgauss electric scooterOPPO Reno7 Pro
+        5GiPhone 13iPhone 13 ProiPhone 13 Pro MaxiPhone 13 MiniGoogle Pixel 6a
+        coversFlipkart QuickAmpere MagnusFlipkart Help CentreOnline Boat
+        StoreCovers from Rs 99Flipkart Track OrdersFlipkart Manage
+        OrdersFlipkart Return OrdersFlipkart Gift Cards StoreFlipkart Axis Bank
+        Credit CardFlipkart Pay Later MOBILES:iPhone 12 64GBiPhone 12 Pro
+        512GBiPhone 12 128GBVivo Y91iVivo Y11Vivo Y15Nothing PhoneVivo Y50Vivo
+        Y12Reno2 FOppo A12Nothing Phone 5gGoogle Pixel 5g PhoneMotorola 5g
+        PhoneRealme 5g SmartphoneApple 5g PhoneIqoo 5g PhonesOneplus 5g
+        PhonesVivo 5g PhonesOppo 5g Smart PhonesOppo F15Oppo A31Samsung
+        A71Samsung A51Samsung A31Realme X2iPhone 11iPhone 11 ProMobile
+        CoverMobile Offersiphone x4G MobileNokia MobileSamsung MobileiphoneOppo
+        MobileVivo Mobile CAMERA:GoPro Action CameraNikon CameraCanon CameraSony
+        CameraCanon DSLRNikon DSLR LAPTOPS:MacBook Pro M2Premium LaptopASUS ROG
+        Strix SCAR 16 (2023) Core i9 13th GenASUS ROG Zephyrus M16 (2023) Core
+        i9 13th GenASUS Zenbook 14 OLED (2022)Realme Book Prime Core i5 11th
+        GenMicrosoft Surface Go Pentium 128GBBranded LaptopsApple LaptopsAcer
+        LaptopsLenovo LaptopsDell LaptopsAsus LaptopsHP LaptopsGaming Laptops2
+        in 1 LaptopsLaptopsDell latest laptops 2022HP latest laptops 2022Infinix
+        INBook Y1 PlusSAMSUNG Galaxy Book312th Gen Intel Core Laptops TVS:Nokia
+        TVPanasonic TVThomson TVMi X ProRealme TVMotorola TVOnePlus TVsLG
+        TVTVSony TVSamsung TVAndroid TelevisionIffalcon TvMi TV LARGE
+        APPLIANCES:TV & Appliances End of Season SaleTelevisionWashing
+        MachinesRefrigeratorsAir ConditionersElectric CookersElectric
+        Jug(Heater) / Travel KettlesInduction CooktopsInverters /
+        stabilizerIrons / Iron BoxMixer Grinder JuicerWet
+        GrindersChimneysMicrowave OvensVacuum CleanersWater PurifierFanMarQ
+        Customer Care CLOTHING:Green bridal lehengaBridal BlouseHalf saree
+        blouse designsDesigner blousesBlouse designsMen ShirtsPlain suit with
+        heavy dupattaHalf saree for kidsCotton saree blouse designsKurta
+        PajamaMen T-ShirtsNew blouse designJeansBridal designer blouseSimple
+        aari work blouse designsBridal banarasi sareesSareeHigh waist
+        pantiesHooded jacket womenDressesHoodie tshirts for menOne piecesHip hop
+        cap for menGroom wedding sherwaniDesigner Salwar SuitsBraAari work
+        blouse hand designsCotton simple blouse designsBanarasi saree blouse
+        designsDesigner KurtisStylish blouse astin designTrack PantBlouse neck
+        designsMen KurtasV neck blouse designsGym WearParty DressesPalazzo
+        SuitsBoys ClothingGlovesNightyMaxi DressesAnarkaliGownsCulottesSalwar
+        SuitsKurtisDesigner SareesLeggingsShortsGeorgette SareesEthnic
+        WearBriefs & TrunksNike WatchesWomen henleyuppada pattu sareesBlouse
+        back designLeopard dressesSleeveless shrugsLatest blouse designsSimple
+        Blouse designs FOOTWEAR:ShoesAdidas ShoesReebok ShoesNike ShoesPuma
+        ShoesBootsBata ShoesWoodland ShoesSkechers ShoesSneakersWomens
+        BootsSports ShoesLoafersSandalsLotto Sports ShoesCasual ShoesWomens
+        Skechers ShoesAsics Sports ShoesFormal ShoesSchool Shoes
+        GROCERIES:PhonePe Grocery VoucherHand WashSoapCashew NutsSunflower
+        OilEggsToilet CleanerHarpic Toilet CleanerDettol SoapMustard
+        OilBiscuitsCheesePatanjali AttaFortune OilAashirvaad AttaTea BEST
+        SELLING ON FLIPKART:Fire-Boltt Ninja Calling Pro Bluetooth Calling
+        Smartwatch 1.69 inch HD Display SmartwatchBest Gas GeyserKitchen
+        GeyserNutri BlendersPortable Air CoolerBest Air CoolerBagsHitachi
+        Refrigerator 3 DoorBooksToysCandlesHelmetsWall ClocksBaby
+        FoodChocolatesCyclesCalculatorsLipsticksMaskVertiv UPSFastrack
+        WatchesWalletsEarringsGold CoinsRealme Pad MiniHandbagsconekt SW2
+        SmartwatchMivi DuoPods a350Speaker Cleaner
+        FURNITURE:FurnitureSofasBedsDining setsWardrobesMattressesTV
+        UnitsTablesChairsShelvesBean BagsOffice ChairsComputer TableOffice
+        TablesRed SofaWakefit BedsWhite SofaWakefit MattressGreen SofaBlack
+        SofaBrown Sofa BGMH:Whey
+        ProteinHomeopathyCricketCyclesFootballsTreadmillsChristmas GiftsFitness
+        AccessoriesOnline GuitarBooks StoreMusical Instrument StoreDabur
+        ChyawanprashBaidyanath ChyawanprashEnergy DrinksToysMilk Drink
+        MixesRakhiChyawanprashIndian FlagProtein Supplements
+      </p>
+    </div>
+  );
+}
